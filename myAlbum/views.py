d2597e1dcf0c7b25e79b40f4dc999e209b90adab
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-07 22:02:53 +0300

    updating url patterns and urls.py files

--- myAlbum/views.py
+++ myAlbum/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from django.http  import HttpResponse,Http404
 
 # Create your views here.
+def homepage(request):
+    """
+    View function to render homepage
+    """
+    return render(request, 'all-gallery/album-homepage.html')
