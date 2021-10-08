5a756010b1d8426a335998b35a7e7d5f93fb525f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-08 12:17:34 +0300

    pushing image class model

--- myAlbum/models.py
+++ myAlbum/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 #Location class model
 class Location(models.Model):
+    """
+    Location class for creating our location instances
+    """
     location_name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -14,7 +17,24 @@ class Location(models.Model):
 
 #Category class model
 class Category(models.Model):
+    """
+    Category class for creating our categories instances
+    """
     category_name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.category_name
+
+#Image class model
+class Image(models.Model):
+    """
+    Image class for creating our image instances
+    """
+    image = models.ImageField(upload_to = 'images/')
+    image_name =models.CharField(max_length=100)
+    image_details =models.TextField()
+    image_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    image_location = models.ForeignKey(Location ,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
