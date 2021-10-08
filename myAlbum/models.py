e2147687d4895848f326c0ddf3fca740bc129b61
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-08 11:53:31 +0300

    pushing category class model

--- myAlbum/models.py
+++ myAlbum/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#Location class model
 class Location(models.Model):
     location_name = models.CharField(max_length=50)
 
@@ -9,3 +11,10 @@ class Location(models.Model):
 
     class Meta:
         ordering = ['location_name']
+
+#Category class model
+class Category(models.Model):
+    category_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.category_name
