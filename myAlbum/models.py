80ebe219079c1efd8f44763ec57d509a8fc27562
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-08 11:48:57 +0300

    pushing location class model

--- myAlbum/models.py
+++ myAlbum/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location_name
+
+    class Meta:
+        ordering = ['location_name']
