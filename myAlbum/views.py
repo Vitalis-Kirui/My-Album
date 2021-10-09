5c4c0d9b164591fe43521059c502a326386cff45
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-09 08:03:24 +0300

    pushing search category view function

--- myAlbum/views.py
+++ myAlbum/views.py
@@ -11,3 +11,15 @@ def homepage(request):
     location=Location.get_locations()
 
     return render(request, 'all-gallery/album-homepage.html', {"images":images,"location":location})
+
+def search_category(request):
+    
+    location=Location.get_locations()
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        search = Image.search_by_category(category)
+        message = f"{category}"
+        return render(request, 'search.html',{"message":message,"category": search,"location":location})
+    else:
+        return render(request, 'search.html')
