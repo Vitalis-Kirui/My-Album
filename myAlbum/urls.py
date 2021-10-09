3e030f29b5110e50857fd7b894a7ea3c2354c551
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-09 12:02:40 +0300

    adding search images by location to the navbar

--- myAlbum/urls.py
+++ myAlbum/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$', views.homepage,name='home'),
     url(r'^search/', views.search_category, name='search_category'),
-    url(r'^location/(?P<location_name>\w+)',views.image_location,name = 'location'),
+    url(r'^location/(?P<location_name>\w+)',views.get_image_location,name = 'location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myAlbum/views.py
+++ myAlbum/views.py
@@ -24,8 +24,8 @@ def search_category(request):
     else:
         return render(request, 'all-gallery/search-results.html')
 
-def image_location(request,location_name):
+def get_image_location(request,location_name):
     location=Location.get_locations()
     image= Image.get_images_by_location(location_name)
     message = f"{location_name}"
-    return render(request, 'all-gallery/image_location.html',{"message":message,"image": image,"location":location})
+    return render(request, 'all-gallery/image-location.html',{"message":message,"image": image,"location":location})
